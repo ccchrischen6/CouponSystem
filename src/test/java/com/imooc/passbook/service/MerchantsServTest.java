9a42f5f5bbe96afbfa11a75a8bf0cd837b9f429b
ccchrischen6 <ccchrischen6@gmail.com>
2020-03-14 00:41:11 -0400

    modified on Mar 14th

--- src/test/java/com/imooc/passbook/service/MerchantsServTest.java
+++ src/test/java/com/imooc/passbook/service/MerchantsServTest.java
@@ -25,10 +25,10 @@ public class MerchantsServTest {
     // if has @Test annotation, adding @Transactional will avoid modification in database
     public void testCreateMerchantServ(){
         CreateMerchantsRequest request = new CreateMerchantsRequest();
-        request.setName("TC");
-        request.setLogoUrl("https://www.tc.edu/");
-        request.setBusinessLicenseUrl("https://www.tc.edu/");
-        request.setPhone("9170001111");
+        request.setName("CU");
+        request.setLogoUrl("https://www.columbia.edu/");
+        request.setBusinessLicenseUrl("https://www.columbia.edu/");
+        request.setPhone("9170001234");
         request.setAddress("NYC");
 
         System.out.println(JSON.toJSONString(merchantsServ.createMerchants(request)));
@@ -40,22 +40,25 @@ public class MerchantsServTest {
     }
 
     /**
-     * DropPassTemplates: {"background":2,"desc":"Desc: CU","end":1583690162112,
-     *                      "hasToken":false,"id":18,"limit":10000,"start":1582829762112,
-     *                      "summary":"Summary: CU","title":"Title: CU"}
+     * DropPassTemplates: {"background":1,"desc":"Desc: CU","end":1584897451400,
+     * "hasToken":true,"id":17,"limit":10000,"start":1583173051399,"summary":"Summary: CU",
+     * "title":"Title: CU"}
      *
-     * */
+     * DropPassTemplates: {"background":1,"desc":"Desc: MIT","end":1584897659803,
+     * "hasToken":false,"id":18,"limit":10000,"start":1583173259802,"summary":"Summary: MIT",
+     * "title":"Title: MIT"}
+     */
     @Test
     public void testDropPassTemplate(){
         PassTemplate passTemplate = new PassTemplate();
         passTemplate.setId(18);
-        passTemplate.setTitle("Title: CU");
-        passTemplate.setSummary("Summary: CU");
-        passTemplate.setDesc("Desc: CU");
+        passTemplate.setTitle("MIT");
+        passTemplate.setSummary("MIT");
+        passTemplate.setDesc("MIT");
         passTemplate.setLimit(10000L);
         passTemplate.setHasToken(false);
-        passTemplate.setBackground(2);
-        passTemplate.setStart(new Date());
+        passTemplate.setBackground(1);
+        passTemplate.setStart(DateUtils.addDays(new Date(), -10));
         passTemplate.setEnd(DateUtils.addDays(new Date(), 10));
         System.out.println(JSON.toJSONString(
                 merchantsServ.dropPassTemplate(passTemplate)
